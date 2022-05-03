be960490172c91c8e697a6ec62d6b39d127fc80c
wolftherai <woltherai@gmail.com>
2022-05-03 17:46:15 +0300

    VUE:Layout routes added

--- vue-admin/src/App.vue
+++ vue-admin/src/App.vue
@@ -1,61 +1,12 @@
 <template>
-  <v-app>
-    <Nav/>
-
-
-<div class="container-fluid">
-  <div class="row">
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div class="position-sticky pt-3">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home" aria-hidden="true"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
-              Dashboard
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
-    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-      <div class="table-responsive">
-        <table class="table table-striped table-sm">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1,001</td>
-              <td>random</td>
-              <td>data</td>
-              <td>placeholder</td>
-              <td>text</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </main>
-  </div>
-</div>
-
-  </v-app>
+  <router-view/>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import Nav from "@/components/Nav.vue"
+import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'App',
-  components: {Nav},
-
+  name: 'App'
 });
 </script>
 <style>
--- vue-admin/src/router/index.ts
+++ vue-admin/src/router/index.ts
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
+import Layout from '@/views/Layout.vue'
 Vue.use(VueRouter)
 
-const routes: Array<RouteConfig> = []
+const routes: Array<RouteConfig> = [
+  {path: '', component: Layout}
+]
 
 const router = new VueRouter({
   mode: 'history',
