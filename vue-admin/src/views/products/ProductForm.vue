bdf65bbd14ba6185de17acde498d85f20ff0f15b
wolftherai <woltherai@gmail.com>
2022-05-05 18:28:07 +0300

    added Product form, git ignore

---
+++ vue-admin/src/views/products/ProductForm.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <form @submit.prevent="submit">
+            <div class="mb-3">
+                <v-text-field label="Title" v-model="title"/>
+            </div>
+            <div class="mb-3">
+                <v-text-field label="OEM part number" v-model="oem_part_number" />
+            </div>             
+            <div class="mb-3">
+                <v-text-field label="Brand" v-model="brand"/>
+            </div>
+
+            <div class="mb-3">
+                <v-text-field label="Manufacturer" v-model="manufacturer"/>
+            </div>                        
+
+
+            <div class="mb-3">
+                <v-text-field label="Description" v-model="description"/>
+            </div>
+            <div class="mb-3">
+                <v-text-field label="Image" v-model="image"/>
+            </div>
+
+            <div class="mb-3">
+                <v-text-field label="Price" type="number" min="0" v-model="price"/>
+            </div>
+            <v-btn type="submit" color="primary" >Save</v-btn>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ProductForm",
+    data() {
+        return {
+            title = '',
+            oem_part_number = '',
+            brand = '',
+            manufacturer = '',
+            description = '',
+            image = '',
+            price = '',
+        }
+    },
+    methods {
+        async submit() {
+            wait axios.post('products'),
+            {
+                title: this.title,
+                title: this.title,
+                title: this.title,
+                title: this.title,
+                title: this.title,
+                title: this.title,
+                title: this.title,
+            }
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
