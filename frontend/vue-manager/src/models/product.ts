ea19bf296eaf34061e730e00427df78de4287373
wolftherai <woltherai@gmail.com>
2022-05-09 03:14:08 +0300

    added product and filter model

---
+++ frontend/vue-manager/src/models/product.ts
@@ -0,0 +1,9 @@
+export class Product {
+    title!: string;
+    oem_part_number!: string;
+    brand!: string;
+    manufacturer!: string;
+    description!: string;
+    image!: string;
+    price!: number;
+}
