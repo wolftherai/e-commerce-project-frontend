65f5d9d2cc8609fddedb0a0df39ae0602818f10e
wolftherai <woltherai@gmail.com>
2022-05-11 08:57:33 +0300

    Product form updated for categories

--- frontend/vue-admin/src/views/products/ProductForm copy.vue
+++ frontend/vue-admin/src/views/products/ProductForm copy.vue
@@ -7,57 +7,7 @@
         </div>
         <div class="mb-3">
             <v-text-field label="OEM part number" v-model="oem_part_number" />
-        </div> 
-
-        <div  style="width:300px">
-    <template>
-        <v-select @change="onChange()"
-            v-model="selectedCategory"
-            :items="categories"
-          
-            item-text="name"
-            item-value="id"
-            label="Category"
-            :value="selectedCategory"
-        
-            return-object/>
-    </template>
-
-        </div>
-
-                <div  style="width:300px">
-    <template>
-        <v-select @change="onChange()"
-            v-model="selectedBrand"
-            :items="brands"
-          
-            item-text="name"
-            item-value="id"
-            label="Brand"
-            :value="selectedBrand"
-        
-            return-object/>
-    </template>
-
-        </div>
-
-                <div  style="width:300px">
-    <template>
-        <v-select @change="onChange()"
-            v-model="selectedManufacturer"
-            :items="manufacturers"
-          
-            item-text="name"
-            item-value="id"
-            label="Manufacturer"
-            :value="selectedManufacturer"
-        
-            return-object/>
-    </template>
-
-        </div>
-
-
+        </div>             
         <div class="mb-3">
             <v-text-field label="Brand" v-model="brand"/>
         </div>
@@ -73,13 +23,19 @@
             <v-text-field label="Image" v-model="image"/>
         </div>
 
+            <select class="form-select" >
+             <option>Select category</option>
+                <option v-for="category in categories" v-bind:value="category.name">
+                {{ category.name }}
+             </option>
+            </select>
+
         <div class="mb-3" style="width:200px">
             <v-text-field label="Price"  type="number" min="0" v-model="price"/>
         </div>
         <v-btn type="submit" color="primary" >Save</v-btn>
         <v-btn  color="red"  style="margin-left:25px" @click="cancel" >Cancel</v-btn>
     </form>
-  
 </div>
 </template>
 <script>
@@ -95,58 +51,24 @@ name: "ProductForm",
             description : '',
             image : '',
             price : '',
-            
-            categories: [],
-            brands: [],
-            manufacturers: [],
-            
-            category: '',
-
-            selectedCategory: { id: '1', name: ''},
-            selectedBrand: { id: '1', name: ''},
-            selectedManufacturer: { id: '1', name: ''},
-
         }
     },
     async mounted() {
-
-
+        const categories = await this.$http.get(`categories/`);
         if(this.$route.params.id) // if id exists
         {
-          
             const {data} = await this.$http.get(`products/${this.$route.params.id}`);
-            console.log(data)
             this.title = data.title;
             this.oem_part_number = data.oem_part_number;
+            this.brand = data.brand;
+            this.manufacturer = data.manufacturer;
             this.description = data.description;
             this.image = data.image;
             this.price = data.price;
             this.description = data.description;
-            this.selectedCategory.id = data.category;
-            this.selectedBrand.id = data.brand;
-            this.selectedManufacturer.id = data.manufacturer;
-
         }
-    }  ,        async  created () {
-                    const catData = await this.$http.get(`categories`);
-                    this.categories = catData.data
-                    console.log(this.categories)
-
-                    const brandData = await this.$http.get(`brands`);
-                    this.brands = brandData.data
-                    console.log(this.brands)
-
-                    const manufacturerData = await this.$http.get(`manufacturers`);
-                    this.manufacturers = manufacturerData.data
-                    console.log(this.manufacturers)
-                   // this.defaultSelected.id = this.category_id
-                },
+    },
     methods: {
-
-            async onChange() {
-//alert(this.selectedCategory.name)
-          },
-
           async cancel() {
             if (confirm('Are you sure? Your changes will be discarded.')){
                 await this.$router.push('/products');
@@ -156,12 +78,11 @@ name: "ProductForm",
             const data = {
                 title: this.title,
                 oem_part_number: this.oem_part_number,
+                brand: this.brand,
+                manufacturer: this.manufacturer,
                 description: this.description,
                 image: this.image,
                 price: this.price,
-                category: this.selectedCategory.id,
-                brand: this.selectedBrand.id,
-                manufacturer: this.selectedManufacturer.id 
             };
             
             if (this.$route.params.id) { // if id exists
